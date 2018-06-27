0bc2dffc459aa621adebc1076f2f2608ac81dd99
Umar Niazi <uhkniazi@gmail.com>
2018-06-27 09:58:04 +0100

    counting for bam files done for michael

--- michaelLeonieBamCounts.R
+++ michaelLeonieBamCounts.R
@@ -5,15 +5,23 @@ library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(GenomicAlignments)
 library(rtracklayer)
 
-csFile = file.choose()
+setwd('dataExternal/')
+csFile = list.files('.', pattern = '*.bam$', recursive = T)
 
 # get the exons into GRangesList object
 oGRLgenes = exonsBy(TxDb.Hsapiens.UCSC.hg38.knownGene, by = 'gene')
 
 # for each of these bam file lists do the counting
-oBamFiles = BamFileList(csFile, index=paste0(csFile, '.bai'))
-mCounts = (assays(summarizeOverlaps(oGRLgenes, oBamFiles, ignore.strand = F, singleEnd=F))$counts)
-save(mCounts, file='Temp/mCounts.rds')
+# for each of these bam file lists do the counting
+lCounts = lapply(csFile, function(bfl){
+  ## create a bamfiles list object
+  oBamFiles = BamFileList(bfl, index=paste0(bfl, '.bai'))
+  return(assays(summarizeOverlaps(oGRLgenes, oBamFiles, ignore.strand = F, singleEnd=F))$counts)
+})
+
+save(lCounts, file='../Temp/lCounts.rds')
+
+mCounts = do.call(cbind, lCounts)
 
 library(org.Hs.eg.db)
 columns(org.Hs.eg.db)
@@ -24,3 +32,5 @@ df = AnnotationDbi::select(org.Hs.eg.db, keys = rownames(mCounts), keytype = 'EN
 identical(rownames(mCounts), df$ENTREZID)
 df = cbind(df, mCounts)
 head(df)
+
+write.csv(df, file='../Temp/counts_all.csv')
